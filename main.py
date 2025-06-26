ca5e06cee4d65f78c54af228ca75d6381b478cf3
gowtham-dd <gowthamd997@gmail.com>
2025-06-26 19:56:45 +0530

    Data Transformtion Done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.WinePredMLflow import logger
 from src.WinePredMLflow.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.WinePredMLflow.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.WinePredMLflow.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -24,6 +26,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
