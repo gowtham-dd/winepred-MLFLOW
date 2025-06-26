7220ecf8ee5d7d88a7b0b96c22136fa0f2d2ce43
gowtham-dd <gowthamd997@gmail.com>
2025-06-26 14:47:55 +0530

    Data Validation Done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from WinePredMLflow import logger
-from WinePredMLflow.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.WinePredMLflow import logger
+from src.WinePredMLflow.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.WinePredMLflow.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -13,3 +15,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
